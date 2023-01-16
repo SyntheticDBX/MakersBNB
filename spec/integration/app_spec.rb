c46cc394b5a4cd007aeca65727ac151f30c416fe
Jessica Lee <jessica.zy.lee@gmail.com>
2023-01-16 16:43:13 +0000

    Initial commit

--- spec/integration/app_spec.rb
+++ spec/integration/app_spec.rb
@@ -19,10 +19,47 @@ describe Application do
 
 
   context 'GET /' do
-    it 'should get the homepage' do
+    it 'displays homepage' do
       response = get('/')
 
       expect(response.status).to eq(200)
+      expect(response.body).to include('Hash-it-out')
+      expect(response.body).to include('Hash it out at our lovely studio in Central London!')
+      expect(response.body).to include('123 Harley St')
+      expect(response.body).to include('London')
+      expect(response.body).to include('United Kingdom')
+      expect(response.body).to include('£150')
     end
   end
+
+  context 'GET /login' do
+    it 'displays login form' do
+      response = get('/login')
+      expect(response.status). to eq 200
+      expect(response.body).to include 'Login'
+    end
+  end
+
+  context 'GET /signup' do
+    it 'displays signup page' do
+      response = get('/signup')
+      expect(response.status).to eq 200
+      expect(response.body).to include 'Signup'
+    end
+  end
+  
+  context 'GET listing/:id' do
+    it 'displays listing ID 2s page' do
+      response = get('/listing/2')      
+      expect(response.status).to eq 200
+      expect(response.body).to include('Not-dodgy mattress')
+      expect(response.body).to include('321 Denmark Street')
+      expect(response.body).to include('Birmingham')
+      expect(response.body).to include('United Kingdom')
+    end
+  end
+
+
+
+
 end
