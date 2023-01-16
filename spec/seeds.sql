d3619aa88d7930c9d2bb462f1d5cf2c3ba0bf2c2
Murat Sertay <118366257+murat-zsertay@users.noreply.github.com>
2023-01-16 16:31:31 +0000

    Create seeds.sql
    Adding sql file

---
+++ spec/seeds.sql
@@ -0,0 +1,44 @@
+CREATE TABLE IF NOT EXISTS users
+(
+    id            SERIAL
+        PRIMARY KEY,
+    first_name    VARCHAR(30),
+    surname       VARCHAR(30),
+    email_address VARCHAR(50),
+    password      VARCHAR(30),
+    username      VARCHAR(30),
+    user_created  DATE
+);
+
+CREATE TABLE IF NOT EXISTS spaces
+(
+    id                  SERIAL
+        PRIMARY KEY,
+    user_id             INTEGER
+        CONSTRAINT spaces_users_id_fk
+            REFERENCES users,
+    "1st_address_line " VARCHAR(30),
+    "2nd_address_line"  VARCHAR(30),
+    city                VARCHAR(20),
+    country             VARCHAR(30),
+    postcode            VARCHAR(10),
+    description         VARCHAR(300),
+    price               money,
+    listing_created     DATE
+);
+
+CREATE TABLE IF NOT EXISTS bookings
+(
+    id                  SERIAL
+        CONSTRAINT bookings_pk
+            PRIMARY KEY,
+    booking_start_date  DATE,
+    booking_end_date    DATE,
+    user_id             INTEGER
+        CONSTRAINT bookings_users_id_fk
+            REFERENCES users,
+    space_id            INTEGER
+        CONSTRAINT bookings_spaces_id_fk
+            REFERENCES spaces,
+    "booking_approved?" BOOLEAN
+);
