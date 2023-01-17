184f342eecdae0418a04b042a014338e05cb8a5b
Murat Sertay <118366257+murat-zsertay@users.noreply.github.com>
2023-01-17 10:22:06 +0000

    Update seeds.sql
    Updated field names and added fields

--- spec/seeds.sql
+++ spec/seeds.sql
@@ -1,44 +1,47 @@
-CREATE TABLE IF NOT EXISTS users
+CREATE TABLE IF NOT EXISTS public.users
 (
-    id            SERIAL
+    id                SERIAL
         PRIMARY KEY,
-    first_name    VARCHAR(30),
-    surname       VARCHAR(30),
-    email_address VARCHAR(50),
-    password      VARCHAR(30),
-    username      VARCHAR(30),
-    user_created  DATE
+    first_name        VARCHAR(30),
+    surname           VARCHAR(30),
+    email_address     VARCHAR(50),
+    password          VARCHAR(30),
+    username          VARCHAR(30),
+    user_created_date DATE
 );
 
-CREATE TABLE IF NOT EXISTS spaces
+CREATE TABLE IF NOT EXISTS public.spaces
 (
     id                  SERIAL
         PRIMARY KEY,
     user_id             INTEGER
         CONSTRAINT spaces_users_id_fk
-            REFERENCES users,
-    "1st_address_line " VARCHAR(30),
-    "2nd_address_line"  VARCHAR(30),
+            REFERENCES public.users,
+    first_address_line  VARCHAR(30),
+    second_address_line VARCHAR(30),
     city                VARCHAR(20),
     country             VARCHAR(30),
     postcode            VARCHAR(10),
     description         VARCHAR(300),
     price               money,
-    listing_created     DATE
+    space_created_date  DATE
 );
 
-CREATE TABLE IF NOT EXISTS bookings
+CREATE TABLE IF NOT EXISTS public.bookings
 (
-    id                  SERIAL
+    id                   SERIAL
         CONSTRAINT bookings_pk
             PRIMARY KEY,
-    booking_start_date  DATE,
-    booking_end_date    DATE,
-    user_id             INTEGER
+    booking_start_date   DATE,
+    booking_end_date     DATE,
+    user_id              INTEGER
         CONSTRAINT bookings_users_id_fk
-            REFERENCES users,
-    space_id            INTEGER
+            REFERENCES public.users,
+    space_id             INTEGER
         CONSTRAINT bookings_spaces_id_fk
-            REFERENCES spaces,
-    "booking_approved?" BOOLEAN
+            REFERENCES public.spaces,
+    "booking_approved?"  BOOLEAN,
+    booking_created_date DATE
 );
+
+
