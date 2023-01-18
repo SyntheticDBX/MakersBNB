03cf771b735846d6f3a1d484ad10d337d5564afc
Jessica Lee <jessica.zy.lee@gmail.com>
2023-01-18 12:42:31 +0000

    First commit

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
+require_relative './lib/space_repository'
 
 class Application < Sinatra::Base
   configure :development do
@@ -7,6 +8,14 @@ class Application < Sinatra::Base
   end
 
   get '/' do
+    return erb(:index)
+  end
+
+  get '/spaces' do
+    repo = SpaceRepository.new
+
+    space = repo.all
+
     return erb(:index)
   end
 end
