cfccfcb5946772427a12e540d745660e80790f78
Murat <murat@sertay.tech>
2023-01-20 06:00:05 +0000

    Rewrote space classes to be more uncoupled

--- lib/space.rb
+++ lib/space.rb
@@ -1,3 +1,18 @@
 class Space
   attr_accessor :id, :name, :user_id, :first_line_address, :second_line_address, :city, :country, :postcode, :description, :space_created_date, :dates_available, :price_per_night
+
+  def initialize(space_hash)
+@id = space_hash['id'].to_i
+@name = space_hash['name']
+@description = space_hash['description']
+@user_id = space_hash['user_id'].to_i
+@first_line_address = space_hash['first_line_address']
+@second_line_address = space_hash['second_line_address']
+@city = space_hash['city']
+@country = space_hash['country']
+@postcode = space_hash['postcode']
+@space_created_date = space_hash['space_created_date']
+@dates_available = space_hash['dates_available']
+@price_per_night = space_hash['price_per_night'].to_f
+  end
 end
--- lib/space_repository.rb
+++ lib/space_repository.rb
@@ -8,60 +8,21 @@ class SpaceRepository
     def all
         sql = "SELECT * FROM spaces"
         result_set = DatabaseConnection.exec_params(sql, [])
-  
-        spaces = []
-
-        results = result_set.each do |record|
-            space = Space.new
-
-            space.id = record['id'].to_i
-            space.name = record['name']
-            space.description = record['description']
-            space.user_id = record['user_id'].to_i
-            space.first_line_address = record['first_line_address']
-            space.second_line_address = record['second_line_address']
-            space.city = record['city']
-            space.country = record['country']
-            space.postcode = record['postcode']
-            space.space_created_date = record['space_created_date']
-            space.dates_available = record['dates_available']
-            space.price_per_night = record['price_per_night'].to_f
-  
-            spaces << space
-        end
-
-        return spaces
+        result_set.map { | space_hash | Booking.new(space_hash)}
     end
 
     def find(id)
         sql = "SELECT * FROM spaces WHERE id = $1"
         result_set = DatabaseConnection.exec_params(sql, [id])
-        record = result_set.to_a[0]
-        space = Space.new
-        space.id = record['id'].to_i
-        space.name = record['name']
-        space.description = record['description']
-        space.user_id = record['user_id'].to_i
-        space.first_line_address = record['first_line_address']
-        space.second_line_address = record['second_line_address']
-        space.city = record['city']
-        space.country = record['country']
-        space.postcode = record['postcode']
-        space.space_created_date = record['space_created_date']
-        space.dates_available = record['dates_available']
-        space.price_per_night = record['price_per_night'].to_f
+        result_set.map { | space_hash | Booking.new(space_hash)}.first
 
-
-
-        return space
     end
 
     def create(space)
-        # current_id = DatabaseConnection.exec_params("SELECT setval('spaces_id_seq', (SELECT max(id) FROM spaces));", []).to_a.first["setval"].to_i
-        # next_id = current_id + 1
+
         sql_params = [space.name, space.description, space.user_id, space.first_line_address, space.second_line_address, space.city, space.country, space.postcode, space.space_created_date,space.dates_available, space.price_per_night]
         sql_query = "INSERT INTO spaces (name, description, user_id, first_line_address, second_line_address, city, country, postcode, space_created_date,dates_available, price_per_night) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11);"
-        result = DatabaseConnection.exec_params(sql_query, sql_params)
+        DatabaseConnection.exec_params(sql_query, sql_params)
         
         return nil
     end
