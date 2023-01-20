8c27c8870a57fe6413d30accf5e9c8da7f953afc
Murat <murat@sertay.tech>
2023-01-20 05:56:28 +0000

    Rewrote booking classes to be more uncoupled

--- lib/booking.rb
+++ lib/booking.rb
@@ -1,3 +1,13 @@
 class Booking
     attr_accessor :id, :booking_start_date, :booking_end_date, :user_id, :space_id, :booking_approved, :booking_created_date
+
+    def initialize(booking_hash)
+@id = booking_hash["id"].to_i
+@booking_start_date = booking_hash["booking_start_date"]
+@booking_end_date = booking_hash["booking_end_date"]
+@user_id = booking_hash["user_id"].to_i
+@space_id = booking_hash["space_id"].to_i
+@booking_approved = booking_hash["booking_approved"]
+@booking_created_date = booking_hash["booking_created_date"]
+    end
 end
--- lib/booking_repository.rb
+++ lib/booking_repository.rb
@@ -2,39 +2,17 @@ require_relative './booking'
 require_relative './database_connection'
 class BookingRepository
     def all
-        bookings = []
-
-        sql = 'SELECT * FROM bookings;'
         params = []
-
+        sql = 'SELECT * FROM bookings;'
         result_set = DatabaseConnection.exec_params(sql, params)
-        result_set.each do |item|
-            booking = Booking.new
-            booking.id = item["id"].to_i
-            booking.booking_start_date = item["booking_start_date"]
-            booking.booking_end_date = item["booking_end_date"]
-            booking.user_id = item["user_id"].to_i
-            booking.space_id = item["space_id"].to_i
-            booking.booking_approved = item["booking_approved"]
-            booking.booking_created_date = item["booking_created_date"]
-            bookings << booking
-        end
-        return bookings
+        result_set.map { | booking_hash | Booking.new(booking_hash)}
     end
 
     def find(id)
         sql = 'SELECT * FROM bookings WHERE id = $1'
         params = [id]
-        item = DatabaseConnection.exec_params(sql, params)
-        booking = Booking.new
-        booking.id = item[0]["id"].to_i
-        booking.booking_start_date = item[0]["booking_start_date"]
-        booking.booking_end_date = item[0]["booking_end_date"]
-        booking.user_id = item[0]["user_id"].to_i
-        booking.space_id = item[0]["space_id"].to_i
-        booking.booking_approved = item[0]["booking_approved"]
-        booking.booking_created_date = item[0]["booking_created_date"]
-        return booking
+        bookings_array = DatabaseConnection.exec_params(sql, params)
+        bookings_array.map { | booking_hash | Booking.new(booking_hash)}.first
     end
 
     def create(booking)
